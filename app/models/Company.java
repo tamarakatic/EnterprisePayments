39db89a3712c48b5fd9430f1a0ca61e611d1a08e
tamarakatic <tamarakatic@gmail.com>
2017-04-28 04:11:20 +0200

    Create company model

---
+++ app/models/Company.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Company extends Model {
+	
+	@Column(length = 30, nullable = false)
+	public String name;
+	
+	@Column(length = 8, nullable = false)
+	public int PIB;
+	
+	@Column(length = 50)
+	public String address;
+	
+	@Column(length = 30)
+	public String mobile;
+	
+	@Column(length = 8, nullable = false)
+	public String JMBG;
+
+//	@OneToMany(mappedBy = "company")
+//	public List<BusinessYear> businessYear;
+	
+	@OneToMany(mappedBy = "company")
+	public List<BusinessPartner> businessPartner;
+	
+	public Company(String naziv, int pib, String adresa, 
+				   String mobile, String jmbg) 
+	{
+		super();
+		this.name = naziv;
+		PIB = pib;
+		this.address = adresa;
+		this.mobile = mobile;
+		JMBG = jmbg;
+	}
+		
+}
