e915dbe0a7c789d22397b728ae2411dd4cc24c7c
tamarakatic <tamarakatic@gmail.com>
2017-04-28 04:51:52 +0200

    Create CRUD and filter for Business Year controller.

--- app/controllers/BusinessYears.java
+++ app/controllers/BusinessYears.java
@@ -1,5 +1,41 @@
 package controllers;
 
-public class BusinessYears {
+import java.util.List;
 
+import models.BusinessYear;
+import play.mvc.Controller;
+
+public class BusinessYears extends Controller {
+
+	public static void show(String mode) {
+		List<BusinessYear> years = BusinessYear.findAll();
+		if (mode == null || mode.equals(""))
+			mode = "edit";
+		render(years, mode);
+	}
+
+	public static void create(BusinessYear year) {
+		year.save();
+		show("add");
+	}
+
+	public static void edit(BusinessYear year) {
+		year.save();
+		show("edit");
+	}
+
+	public static void filter(BusinessYear year) {
+		List<BusinessYear> years = BusinessYear
+				.find("byYearLikeAndActiveLike", "%" + year.year + "%", 
+												 "%" + year.active + "%").fetch();
+		renderTemplate("BusinessYears/show.html", "edit", years);
+	}
+
+	public static void delete(Long id) {
+		if (id != null) {
+			BusinessYear year = BusinessYear.findById(id);
+			year.delete();
+		}
+		show("edit");
+	}
 }
