2a17577d17fc9e3fef653796f2e10ba071e5a8ce
tamarakatic <tamarakatic@gmail.com>
2017-05-04 19:48:36 +0200

    Create next form for children from the root model

--- app/controllers/Companies.java
+++ app/controllers/Companies.java
@@ -2,6 +2,8 @@ package controllers;
 
 import java.util.List;
 
+import models.BusinessPartner;
+import models.BusinessYear;
 import models.Company;
 import play.mvc.Controller;
 
@@ -38,6 +40,23 @@ public class Companies extends Controller {
 		if (id != null){
 			Company company = Company.findById(id);
 			company.delete();			
+		}
+		show("edit");
+	}
+	
+	public static void business_partner(Long id) {
+		System.out.println("Usao partner " + id);
+		if (id != null) {
+			List<BusinessPartner> businessPartners = BusinessPartner.find("byCompany_id", id).fetch();
+			renderTemplate("Companies/business_partners.html" ,businessPartners);
+		}
+		show("edit");
+	}
+	
+	public static void business_year(Long id) {
+		if (id != null) {
+			List<BusinessYear> businessYears = BusinessYear.find("byCompany_id", id).fetch();
+			renderTemplate("Companies/business_years.html" ,businessYears);
 		}
 		show("edit");
 	}
