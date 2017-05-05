7e780164e2b62458f182dbc606593b3e3e521cd5
AndrijanaStefanovic <anja.stef@gmail.com>
2017-05-06 01:51:45 +0200

    Created Invoice model, controller, view. Invoice CRUD operations.
    Combo Zoom and modal zoom for Invoices.

---
+++ app/controllers/Invoices.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import java.util.List;
+
+import models.BusinessPartner;
+import models.BusinessYear;
+import models.Company;
+import models.Invoice;
+import play.mvc.Controller;
+
+public class Invoices extends Controller {
+
+	public static void show(String mode){
+		List<Invoice> invoices = Invoice.findAll();
+		if (mode == null || mode.equals(""))
+			mode = "edit";
+		
+		List<Company> companies = Company.findAll();
+		List<BusinessYear> businessYears = BusinessYear.findAll();
+		List<BusinessPartner> businessPartners = BusinessPartner.findAll();
+		renderTemplate("Invoices/show.html", mode, invoices, companies, businessYears, businessPartners);	
+		
+		
+		//render(invoices, mode);
+	}
+	
+	public static void create(Invoice invoice) {
+		Company company = Company.findById(invoice.company.id);
+		invoice.company = company;
+		invoice.businessYear = BusinessYear.findById(invoice.businessYear.id);
+		invoice.businessPartner = BusinessPartner.findById(invoice.businessPartner.id);
+		invoice.save();	
+		show("add");
+	}
+	
+	public static void edit(Invoice invoice) {
+		invoice.save();
+		show("edit");
+	}
+	
+	public static void delete(Long id) {
+		if (id != null){
+			Invoice invoice = Invoice.findById(id);
+			invoice.delete();			
+		}
+		show("edit");
+	}
+	
+	public static void filter(Invoice invoice) {		
+		System.out.println(invoice.number);
+		List<Invoice> invoices = Invoice.find("byNumberAndDateOfInvoiceAndDateOfValueAndBasisAndTax", 
+												 invoice.number,
+												 invoice.dateOfInvoice,
+												 invoice.dateOfValue,
+												 invoice.basis,
+												 invoice.tax).fetch();
+		renderTemplate("Invoices/show.html", "edit", invoices);		
+	}
+}
