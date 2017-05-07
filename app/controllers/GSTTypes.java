eed0e6b51a573c088224c12d26ff55ce28064228
unknown <acafacafunnyface@gmail.com>
2017-05-07 13:17:34 +0200

    Added CRUD for GSTType

---
+++ app/controllers/GSTTypes.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import java.util.List;
+
+import models.BusinessPartner;
+import models.Company;
+import models.GSTType;
+import play.mvc.Controller;
+
+public class GSTTypes extends Controller{
+
+	public static void show(String mode) {
+		List<GSTType> gsttypes = GSTType.findAll();
+		if (mode == null || mode.equals(""))
+			mode = "edit";
+		render(gsttypes, mode);
+	}
+	
+	public static void create(GSTType gsttype) {
+		gsttype.save();		
+		show("add");
+	}
+	
+	public static void edit(GSTType gsttype) {
+		gsttype.save();
+		show("edit");		
+	}
+	
+	public static void filter(GSTType gsttype) {		
+		List<GSTType> gsttypes = GSTType.find("name = ?", gsttype.name).fetch();
+		renderTemplate("GSTTypes/show.html", "edit", gsttypes);		
+	}
+	
+	public static void delete(Long id) {
+		if (id != null){
+			GSTType gsttype = GSTType.findById(id);
+			gsttype.delete();			
+		}
+		show("edit");
+	}
+	
+}
