84f69c0c6f497835f7bfc2d4173332fdfdf67ec4
tamarakatic <tamarakatic@gmail.com>
2017-06-06 04:09:02 +0200

    Redirect to https does not work

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,31 @@ import models.*;
 
 @With(Secure.class)
 public class Application extends Controller {
+	
+//    @Before
+//    public static void redirectToHttps() {
+//        if (!request.secure && request.headers.get("x-forwarded-proto") != null) {
+//            request.secure = request.headers.get("x-forwarded-proto").values.contains("https");
+//        }
+//
+//        if (!request.secure) {
+//            redirect(redirectHostHttps() + request.url);
+//        }
+//    }
+//
+//    @Util
+//    public static String redirectHostHttps() {
+//        if (Play.id.equals("dev")) {
+//            String httpsPort = (String) Play.configuration.get("https.port");
+//            return "https://" + request.host.split(":")[0] + ":" + httpsPort; 
+//        } else {
+//            if (request.host.endsWith("domain.com")) {
+//                return "https://secure.domain.com";
+//            } else {
+//                return "https://" + request.host;
+//            }
+//        }
+//    }    
 
     public static void index() {
     	String user = Security.connected();
