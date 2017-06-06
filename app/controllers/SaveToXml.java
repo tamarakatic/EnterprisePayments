c710bec3a2f0ab8ddc556d49e58670d1584c8e8e
tamarakatic <tamarakatic@gmail.com>
2017-06-06 04:11:23 +0200

    Create saveToXml method to create xml and send via http

---
+++ app/controllers/SaveToXml.java
@@ -0,0 +1,167 @@
+package controllers;
+
+import java.io.File;
+import java.io.StringWriter;
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+import java.util.List;
+
+import javax.crypto.SecretKey;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.commons.lang.time.DateFormatUtils;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import cryptoXML.XMLReadCertificateAndSignDoc;
+import cryptoXML.XMLUtility;
+import models.Invoice;
+import models.InvoiceItem;
+import play.libs.WS;
+import play.libs.WS.HttpResponse;
+import sun.misc.BASE64Encoder;
+
+public abstract class SaveToXml {
+	
+	static void saveToXML(Long id, Invoice invoice, List<InvoiceItem> items) {
+		String invoiceNumber= Integer.toString(invoice.number);
+		String bussinessPartner = invoice.businessPartner.name;
+		String businessYear = Integer.toString(invoice.businessYear.year);
+	    String companyName = invoice.company.name;
+	    String invoiceDate = DateFormatUtils.format(invoice.dateOfInvoice, "yyyy-MM-dd HH:mm:SS");
+	    String invoiceValueDate = DateFormatUtils.format(invoice.dateOfValue, "yyyy-MM-dd HH:mm:SS");
+	    String basis = Double.toString(invoice.basis);
+	    String tax = Double.toString(invoice.tax);
+	    String total = Double.toString(invoice.total);
+
+	  try {
+	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	         Document doc = dBuilder.newDocument();
+	   
+	         Element topElement = doc.createElement("invoices");	         
+	         doc.appendChild(topElement);
+
+	         Element rootElement = doc.createElement("invoices_data");
+	         topElement.appendChild(rootElement);
+	                  
+	         Element invoicesId = doc.createElement("id");
+	         invoicesId.appendChild(doc.createTextNode(id.toString()));
+	         
+	         Element company = doc.createElement("company");
+	         company.appendChild(doc.createTextNode(companyName));
+	         
+	         Element partner = doc.createElement("partner");
+	         partner.appendChild(doc.createTextNode(bussinessPartner));
+	         
+	         Element year = doc.createElement("year");
+	         year.appendChild(doc.createTextNode(businessYear));
+	         
+	         Element number = doc.createElement("number");
+	         number.appendChild(doc.createTextNode(invoiceNumber));
+	         
+	         Element dateOfInvoice = doc.createElement("invoiceDate");
+	         dateOfInvoice.appendChild(doc.createTextNode(invoiceDate));
+	         
+	         Element dateOfValue = doc.createElement("valueDate");
+	         dateOfValue.appendChild(doc.createTextNode(invoiceValueDate));
+	         
+	         Element basicValue = doc.createElement("basicValue");
+	         basicValue.appendChild(doc.createTextNode(basis));
+	         
+	         Element taxValue = doc.createElement("taxValue");
+	         taxValue.appendChild(doc.createTextNode(tax));
+	         
+	         Element sumValue = doc.createElement("sumValue");
+	         sumValue.appendChild(doc.createTextNode(total));
+	         
+	         rootElement.appendChild(invoicesId);
+	         rootElement.appendChild(company);
+	         rootElement.appendChild(partner);
+	         rootElement.appendChild(year);
+	         rootElement.appendChild(number);
+	         rootElement.appendChild(dateOfInvoice);
+	         rootElement.appendChild(dateOfValue);
+	         rootElement.appendChild(basicValue);
+	         rootElement.appendChild(taxValue);
+	         rootElement.appendChild(sumValue);
+	         
+	         if (items != null) {
+				 for (InvoiceItem item : items) {
+					 Element invoice_element =  doc.createElement("invoice");
+					 Element invoice_amount =  doc.createElement("amount");
+					 invoice_amount.appendChild(doc.createTextNode(Double.toString(item.amount)));
+					 Element invoice_total =  doc.createElement("total");
+					 invoice_total.appendChild(doc.createTextNode(Double.toString(item.total)));
+					 Element invoice_price =  doc.createElement("price");
+					 invoice_price.appendChild(doc.createTextNode(Double.toString(item.price)));
+					 invoice_element.appendChild(invoice_amount);
+					 invoice_element.appendChild(invoice_total);
+					 invoice_element.appendChild(invoice_price);
+					 topElement.appendChild(invoice_element);
+				 }
+	         }
+	         	         
+	         XMLReadCertificateAndSignDoc xml = new XMLReadCertificateAndSignDoc();
+	         XMLUtility xmlUtility = new XMLUtility();
+	 		 String keyStoreFile = "./data/keystores/primer.jks";
+	 		  		
+	 		 System.out.println("===== XML Encryption & Signature algoritma =====");
+	 		 SecretKey secretKey = xmlUtility.generateDataEncryptionKey();
+	 		 System.out.println("\n===== Generisan kljuc =====");
+	 		 System.out.println(new BASE64Encoder().encode((secretKey.getEncoded())));
+	 		
+	 		 Certificate cert = xml.readCertificate(keyStoreFile, "primer", "primer");
+	 		 System.out.println("\n===== Sifrovanje XML dokumenta =====");
+	 		 doc = xmlUtility.encrypt(doc, secretKey, cert);
+	 		
+	 		 System.out.println("\n===== Potpisivanje XML dokumenta =====");
+	 		 PrivateKey privateKey = xmlUtility.readPrivateKey(keyStoreFile, "primer", "primer", "primer");
+	 		 doc = xml.signDocument(doc, privateKey, cert);
+	 		  		
+	 		 System.out.println("\n===== Transfer XML dokumenta od firme A do firme B =====");	
+	 		 
+	 	     System.out.println("\n===== Provera validnosti digitalnog potpisa =====");	 	     
+
+	 	     boolean res = xmlUtility.verifySignature(doc);
+			 if(res) {
+				System.out.println("\n===== Potpis je validan, dokument se desifruje =====");
+				doc = xmlUtility.decrypt(doc, privateKey);
+				System.out.println("\n===== Desifrovanje zavrseno, firma B je primila dokument =====");					
+			 
+			 } else {
+				System.out.println("\n===== Potpis nije validan, dokument se odbacuje =====");
+			 }
+
+			 DOMSource srs = new DOMSource(doc);
+	 		 StreamResult test = new StreamResult(new File("test.xml"));
+	 		  		
+	         TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	         Transformer transformer = transformerFactory.newTransformer();
+	         DOMSource source = new DOMSource(doc);
+	         StringWriter writer = new StringWriter();
+	         StreamResult result = new StreamResult(writer);     
+
+	         transformer.transform(srs, test);
+	         transformer.transform(source, result);	   
+	     
+	         HttpResponse response = WS.url("http://localhost:8100/invoices")
+	        		 .setHeader("Content-Type", "application/xml")
+	        		 .setHeader("PrivateKey", privateKey.toString())
+	        		 .body(writer.toString())
+	        		 .post();
+	         
+            System.out.println("Responseeeeeeee: " + response.getString());
+     
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	}
+
+}
